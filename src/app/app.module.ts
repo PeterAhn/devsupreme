3920e19549edf87d6dd823a37b6998d8a7eb4712
peterahn <xion486@gmail.com>
2017-10-05 00:07:12 +0900

    초기 버전

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,22 +4,27 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS  } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
-
+import { AuthGuard } from './auth.guard';
 import { UtilService } from './util.service';
 import { AuthService } from './auth.service';
 import { RequestInterceptor } from './request-interceptor.service';
+import { UserService } from './user.service'; //*
 
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { Error404Component } from './error404/error404.component';
 import { LoginComponent } from './login/login.component';
+import { UserNewComponent } from './user-new/user-new.component';
+import { UserIndexComponent } from './user-index/user-index.component'; //2
 
 @NgModule({
   declarations: [
     AppComponent,
     WelcomeComponent,
     Error404Component,
-    LoginComponent
+    LoginComponent,
+    UserNewComponent,
+    UserIndexComponent, //2
   ],
   imports: [
     BrowserModule,
@@ -34,8 +39,10 @@ import { LoginComponent } from './login/login.component';
       useClass: RequestInterceptor,
       multi: true,
     },
+    AuthGuard,
     UtilService,
     AuthService,
+    UserService,
   ],
   bootstrap: [AppComponent]
 })
